1049433829cd690cfb4da8835740a64f7751ea74
Michael Degginger <49601011+therandomfool@users.noreply.github.com>
2019-07-02 13:43:49 -0600

    Add files via upload
    added new numbers JS

---
+++ index.js
@@ -0,0 +1,17 @@
+// assign random numbers to a & b
+let a = Math.floor(Math.random() * 100);
+let b = Math.floor(Math.random() * 100);
+
+// set add, multiply, division and subtraction
+let add = a + b;
+let mult = a * b;
+let div = (a / b);
+let sub = a - b;
+
+
+
+//  display result in console
+console.log(`ADDITION ${a} + ${b} = ${add}`);
+console.log(`MULTIPLICATION ${a} * ${b} = ${mult}`);
+console.log(`DIVISION ${a} / ${b} = ${div.toFixed(2)}`);
+console.log(`SUBTRACTION ${a} - ${b} = ${sub}`);
