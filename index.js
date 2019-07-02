9b837152a2bbf825c29d62ca7f6df0e85b19ac5b
Michael Degginger <49601011+therandomfool@users.noreply.github.com>
2019-07-02 14:34:31 -0600

    Add files via upload
    number manipulation

--- index.js
+++ index.js
@@ -9,9 +9,17 @@ let div = (a / b);
 let sub = a - b;
 
 
-
 //  display result in console
 console.log(`ADDITION ${a} + ${b} = ${add}`);
 console.log(`MULTIPLICATION ${a} * ${b} = ${mult}`);
 console.log(`DIVISION ${a} / ${b} = ${div.toFixed(2)}`);
 console.log(`SUBTRACTION ${a} - ${b} = ${sub}`);
+
+
+let x = 0;
+
+
+for (let z = 0; z < 20; z++) {
+    x++;
+    console.log(x);
+}
